c5e232c99e7989bd8908436addad0f5389e4edd0
Mrshaw <269719110@qq.com>
2019-03-13 11:06:13 +0800

    '增加home.vue'

---
+++ src/components/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Home-header></Home-header>
+  </div>
+</template>
+<script>
+import HomeHeader from './pages/header'
+export default {
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style>
+</style>

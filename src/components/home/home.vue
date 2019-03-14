20056ede0ee1d89e15b9b99143551799c18c414a
Mrshaw <269719110@qq.com>
2019-03-14 15:18:44 +0800

    '增加定位板块'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -3,18 +3,20 @@
     <Home-header></Home-header>
     <Sweiper></Sweiper>
     <Icon></Icon>
+    <Iocation></Iocation>
   </div>
 </template>
 <script>
 import HomeHeader from './pages/header'
 import Sweiper from './pages/Sweiper'
 import Icon from './pages/Icons'
-
+import Iocation from './pages/location'
 export default {
   components: {
     HomeHeader,
     Sweiper,
-    Icon
+    Icon,
+    Iocation
   }
 }
 </script>
---
+++ src/components/home/pages/location.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="location  border-top" >
+    <div class="divs border-right"><span class="iconfont">&#xe63f;</span>定位失败</div>
+    <div class="divs"><span class="iconfont">&#xe689;</span>玩水季</div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.location{
+    position: relative;
+    display: flex;
+    font-size: .2rem;
+}
+.location .divs{
+    flex:1;
+    width:50%;
+    height:.7rem;
+    text-align: center;
+    line-height: 0.8rem;
+}
+</style>
